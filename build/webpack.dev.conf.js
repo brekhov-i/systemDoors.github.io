51d7fda6827063dadb5a87d235813c067103803c
Илья Брехов <ilabrehov@MacBook-Air-Ila.local>
2020-07-04 13:16:06 +0300

    Начальный шаблон сайта

---
+++ build/webpack.dev.conf.js
@@ -0,0 +1,30 @@
+const webpack = require("webpack");
+const merge = require("webpack-merge");
+const baseWebpackConfig = require("./webpack.base.conf");
+
+const devWebpackConfig = merge(baseWebpackConfig, {
+  // DEV settings gonna be here
+  mode: "development",
+
+  devtool: "cheap-module-source-map",
+
+  //contentBase: baseWebpackConfig.externals.paths.dist,
+
+  devServer: {
+    overlay: {
+      warnings: true,
+      errors: true,
+    },
+    port: 8082,
+    //host: '192.168.0.105'
+  },
+  plugins: [
+    new webpack.SourceMapDevToolPlugin({
+      filename: "[file].[hash].map",
+    }),
+  ],
+});
+// export devWebpackConfig
+module.exports = new Promise((resolve, reject) => {
+  resolve(devWebpackConfig);
+});
---
+++ src/index.js
@@ -0,0 +1,6 @@
+//css
+import './assets/scss/style.scss';
+
+
+//js
+import './assets/js/index';

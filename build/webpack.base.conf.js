2e6f9840e6fbb62b62cc9a850d5ecad0f9159ec1
Илья Брехов <ilabrehov@MacBook-Air-Ila.local>
2020-07-05 13:59:49 +0300

    Создал блок катигорий на главной странице

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -1,149 +1,142 @@
-const path = require('path')
+const path = require("path");
 const fs = require("fs");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 const PATHS = {
-    src: path.join(__dirname, "../src"),
-    public: path.join(__dirname, "../public"),
-    assets: "assets/"
+  src: path.join(__dirname, "../src"),
+  public: path.join(__dirname, "../public"),
+  assets: "assets/",
 };
 
 //PUG пути "ДЛЯ РАБОТЫ С PUG НУЖНО СОЗДАТЬ ПАПКУ 'pug/pages'"
-// const PAGES_DIR = `${PATHS.src}/pug/pages/`
-// const PAGES = fs
-//     .readdirSync(PAGES_DIR)
-//     .filter(fileName => fileName.endsWith('.pug'));
-
-const PAGES_DIR = `${PATHS.src}`
+const PAGES_DIR = `${PATHS.src}/pug/pages/`;
 const PAGES = fs
-    .readdirSync(PAGES_DIR)
-    .filter(fileName => fileName.endsWith('.html'));
+  .readdirSync(PAGES_DIR)
+  .filter((fileName) => fileName.endsWith(".pug"));
 
 module.exports = {
-    externals: {
-        paths: PATHS
+  externals: {
+    paths: PATHS,
+  },
+  entry: {
+    index: PATHS.src + "/index.js",
+  },
+  output: {
+    filename: `${PATHS.assets}js/[name].[contenthash].js`,
+    path: PATHS.public,
+    publicPath: "/",
+  },
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          name: "vendors",
+          test: /node_modules/,
+          chunks: "all",
+          enforce: true,
+        },
+      },
     },
-    entry: {
-        index: PATHS.src + '/index.js',
-    },
-    output: {
-        filename: `${PATHS.assets}js/[name].[contenthash].js`,
-        path: PATHS.public,
-        publicPath: "/"
-    },
-    optimization: {
-        splitChunks: {
-            cacheGroups: {
-                vendor: {
-                    name: 'vendors',
-                    test: /node_modules/,
-                    chunks: 'all',
-                    enforce: true
-                }
-            }
-        }
-    },
-    plugins: [
-        new MiniCssExtractPlugin({
-            filename: `${PATHS.assets}css/[name].[contenthash].css`
-        }),
-        new CopyWebpackPlugin([
-            { from: `${PATHS.src}/${PATHS.assets}img`, to: `${PATHS.assets}img` },
-            { from: `${PATHS.src}/${PATHS.assets}fonts`, to: `${PATHS.assets}fonts` },
-            { from: `${PATHS.src}/static`, to: "" }
-        ]),
-        //PUG преобразование в html
-        // ...PAGES.map(page => new HtmlWebpackPlugin({
-        //     template: `${PAGES_DIR}/${page}`,
-        //     filename: `./${page.replace(/\.pug/,'.html')}`,
-        //     chunks: [`${page.replace(/\.pug/,'')}`, 'vendors'],
-        // })),
-        ...PAGES.map(page => new HtmlWebpackPlugin({
-            template: `${PAGES_DIR}/${page}`,
-            filename: `./${page}`,
-            chunks: [`${page.replace(/\.html/,'')}`, 'vendors'],
-        })),
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: `${PATHS.assets}css/[name].[contenthash].css`,
+    }),
+    new CopyWebpackPlugin([
+      { from: `${PATHS.src}/${PATHS.assets}img`, to: `${PATHS.assets}img` },
+      { from: `${PATHS.src}/${PATHS.assets}fonts`, to: `${PATHS.assets}fonts` },
+      { from: `${PATHS.src}/static`, to: "" },
+    ]),
+    //PUG преобразование в html
+    ...PAGES.map(
+      (page) =>
+        new HtmlWebpackPlugin({
+          template: `${PAGES_DIR}/${page}`,
+          filename: `./${page.replace(/\.pug/, ".html")}`,
+          chunks: [`${page.replace(/\.pug/, "")}`, "vendors"],
+        })
+    ),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+      {
+        // Fonts
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "file-loader",
+        options: {
+          name: "[name].[ext]",
+        },
+      },
+      {
+        // images / icons
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: "file-loader",
+        options: {
+          name: "[name].[ext]",
+        },
+      },
+      {
+        // scss
+        test: /\.s[ac]ss$/i,
+        use: [
+          "style-loader",
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: { sourceMap: true },
+          },
+          {
+            loader: "postcss-loader",
+            options: {
+              sourceMap: true,
+              config: { path: `build/postcss.config.js` },
+            },
+          },
+          {
+            loader: "sass-loader",
+            options: { sourceMap: true },
+          },
+        ],
+      },
+      {
+        // css
+        test: /\.css$/,
+        use: [
+          "style-loader",
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: { sourceMap: true },
+          },
+          {
+            loader: "postcss-loader",
+            options: {
+              sourceMap: true,
+              config: { path: `build/postcss.config.js` },
+            },
+          },
+        ],
+      },
+      //PUG-loader
+      {
+        test: /\.pug$/,
+        loader: "pug-loader",
+        options: {
+          pretty: true,
+        },
+      },
     ],
-    module: {
-        rules: [
-            {
-                test: /\.m?js$/,
-                exclude: /(node_modules|bower_components)/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env']
-                    }
-                }
-            },
-            {
-                // Fonts
-                test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
-                loader: "file-loader",
-                options: {
-                    name: "[name].[ext]"
-                }
-            },
-            {
-                // images / icons
-                test: /\.(png|jpg|gif|svg)$/,
-                loader: "file-loader",
-                options: {
-                    name: "[name].[ext]"
-                }
-            },
-            {
-                // scss
-                test: /\.s[ac]ss$/i,
-                use: [
-                    "style-loader",
-                    MiniCssExtractPlugin.loader,
-                    {
-                        loader: "css-loader",
-                        options: { sourceMap: true }
-                    },
-                    {
-                        loader: "postcss-loader",
-                        options: {
-                            sourceMap: true,
-                            config: { path: `build/postcss.config.js` }
-                        }
-                    },
-                    {
-                        loader: "sass-loader",
-                        options: { sourceMap: true }
-                    }
-                ]
-            },
-            {
-                // css
-                test: /\.css$/,
-                use: [
-                    "style-loader",
-                    MiniCssExtractPlugin.loader,
-                    {
-                        loader: "css-loader",
-                        options: { sourceMap: true }
-                    },
-                    {
-                        loader: "postcss-loader",
-                        options: {
-                            sourceMap: true,
-                            config: { path: `build/postcss.config.js` }
-                        }
-                    }
-                ]
-            },
-            //PUG-loader
-            // {
-            //     test: /\.pug$/,
-            //     loader: 'pug-loader',
-            //     options: {
-            //         pretty: true
-            //     }
-            // }
-        ]
-    }
-}
+  },
+};
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 //css
-import './assets/scss/style.scss';
-
+import "./assets/scss/style.scss";
+import "./assets/scss/pages/index.scss";
 
 //js
-import './assets/js/index';
+import "./assets/js/index";
